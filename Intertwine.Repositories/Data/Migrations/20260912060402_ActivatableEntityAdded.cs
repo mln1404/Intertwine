f2ff051a63698d2529beac6dd7427933a0241fdb
mln1404 <noel.merricklance@gmail.com>
2026-09-12 14:04:42 +0800

    Migration applied

---
+++ Intertwine.Repositories/Data/Migrations/20260912060402_ActivatableEntityAdded.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intertwine.Repositories.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActivatableEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_ApplicationUserId",
+                table: "UserProfiles");
+
+            migrationBuilder.RenameColumn(
+                name: "ApplicationUserId",
+                table: "UserProfiles",
+                newName: "IdentityUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserProfiles_ApplicationUserId",
+                table: "UserProfiles",
+                newName: "IX_UserProfiles_IdentityUserId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "UserProfiles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Questions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "DailyQuestions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "DailyQuestions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "DailyQuestions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "DailyQuestions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Categories",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Answers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Answers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Answers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Answers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Answers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_IdentityUserId",
+                table: "UserProfiles",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_IdentityUserId",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "DailyQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "DailyQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "DailyQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "DailyQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Answers");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Answers");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "Answers");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Answers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Answers");
+
+            migrationBuilder.RenameColumn(
+                name: "IdentityUserId",
+                table: "UserProfiles",
+                newName: "ApplicationUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserProfiles_IdentityUserId",
+                table: "UserProfiles",
+                newName: "IX_UserProfiles_ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_ApplicationUserId",
+                table: "UserProfiles",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
